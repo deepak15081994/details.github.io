360c6e373ae720c044f98ea767c77124a7c1782c
deepak15081994 <deepak.msn01@gmail.com>
2023-06-27 13:30:19 +0530

    My Portfolio Website

---
+++ template2/js/script.js
@@ -0,0 +1,16 @@
+//Sticky Navigation Menu JS
+
+let nav=document.querySelector("nav");
+let scrollBtn=document.querySelector(".scroll-button a");
+
+let val;
+
+window.onscroll=function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+        scrollBtn.style.display="block";
+    }else{
+        nav.classList.remove("sticky");
+        scrollBtn.style.display="none"; 
+    }
+}
